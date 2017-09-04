ae2cec80293453bafed3ed5be7631cdd0387ba64
Alexey Evdokimov <uncle-arthy@users.noreply.github.com>
2017-09-04 22:45:56 +0500

    Step back from module style formatting
    get rid from "if __main__..." stuff for now

--- mfrl.py
+++ mfrl.py
@@ -4,23 +4,60 @@ __author__ = 'Alexei Evdokimov'
 
 import tdl
 
+# actual size of the window
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
 
-def main():
-    screen_width = 80
-    screen_height = 50
-
-    tdl.setFont('arial12x12.png', greyscale=True, altLayout=True)
-
-    console = tdl.init(screen_width, screen_height, title='MFRL v0.1', fullscreen=False)
-
-    playerx = screen_width//2
-    playery = screen_height//2
-
-    while not tdl.event.is_window_closed():
-        console.draw_char(1, 1, '@', bg=None, fg=(255, 255, 255))
-
-        tdl.flush()
+LIMIT_FPS = 20  # 20 frames-per-second maximum
 
 
-if __name__ == '__main__':
-    main()
+def handle_keys():
+    global playerx, playery
+
+    # turn-based
+    user_input = tdl.event.key_wait()
+
+    if user_input.key == 'ENTER' and user_input.alt:
+        # Alt+Enter: toggle fullscreen
+        tdl.set_fullscreen(not tdl.get_fullscreen())
+
+    elif user_input.key == 'ESCAPE':
+        return True  # exit game
+
+    # movement keys
+    if user_input.key == 'UP':
+        playery -= 1
+
+    elif user_input.key == 'DOWN':
+        playery += 1
+
+    elif user_input.key == 'LEFT':
+        playerx -= 1
+
+    elif user_input.key == 'RIGHT':
+        playerx += 1
+
+
+#############################################
+# Initialization & Main Loop                #
+#############################################
+
+tdl.set_font('cp437_12x12.png', greyscale=True, altLayout=False)
+console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Roguelike", fullscreen=False)
+tdl.setFPS(LIMIT_FPS)
+
+playerx = SCREEN_WIDTH // 2
+playery = SCREEN_HEIGHT // 2
+
+while not tdl.event.is_window_closed():
+
+    console.draw_char(playerx, playery, '@', bg=None, fg=(255, 255, 255))
+
+    tdl.flush()
+
+    console.draw_char(playerx, playery, ' ', bg=None)
+
+    # handle keys and exit game if needed
+    exit_game = handle_keys()
+    if exit_game:
+        break
