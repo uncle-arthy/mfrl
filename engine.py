36a18324fbb733e72f8c8114ebeaaf55cba2d3e6
Alexey Evdokimov <uncle.arthy@gmail.com>
2017-09-05 13:20:53 +0500

    Once again from start

---
+++ engine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = 'Alexei Evdokimov'
+'''
+Change tutorial from RogueBasin to rogueliketutorials.com
+More Pythonic style and seems more OOP-ish ))
+'''
+
+import tdl
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
